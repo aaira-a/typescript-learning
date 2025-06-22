55236d55a7cdee50e794d1cdfba604b901944945
Aaira Alias <aaairaalias@gmail.com>
2025-06-22 17:26:25 +0800

    add initial greet function

--- hello.ts
+++ hello.ts
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+ 
+greet("Brendan");
