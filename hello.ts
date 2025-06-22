b849715b9613246a1d2523e652236ffe1323bd41
Aaira Alias <aaairaalias@gmail.com>
2025-06-22 17:38:22 +0800

    add todatestring for Date interface in string concat

--- hello.ts
+++ hello.ts
@@ -1,6 +1,6 @@
 // This is an industrial-grade general-purpose greeter function:
 function greet(person:string, date:Date) {
-  console.log(`Hello ${person}, today is ${date}!`);
+  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
  
 greet("Maddison", new Date());
